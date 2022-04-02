6e7cb47127854fdfd5b9df110b640e0f64494adb
brn <brn.gdsn@gmail.com>
2022-04-02 10:11:18 -0400

    added pg

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ oauth2
 `openssl genrsa 2048 -out private.pem`
 
 `openssl rsa -in private.pem -pubout -outform PEM -out public.pem`
+
+```
+/**
+ curl localhost:12000/api/v2/eg-resource \
+  -H "Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlZWY5YmQwNjBhMTNjOGVhNGYzNTY4MWQ4MWU4MWI3MTMyYTUzNTNjYmUzNzBmNjk4NjFkMjM1NzRlZmFlNjk0MzhlMTg4NTA0NzMyNGUyOTQwYmQ2ZGIyMTQ5NzkxYTIiLCJleHAiOjE2NDg3MzM4MDksImlhdCI6MTY0ODczMzcwOX0.X_BCxCR9IVAYw-vPkRPCSgGu9thTowEDGq1_I4VBmjX8BmZDEUuxh6RG9-lk7dHGH87xNa8yX0KacpKiidHF9k4Fz0A3S3ng8c71MT--r8Ku0YAByUEU-nTUmRhrQDErndvl6gwQ39C7H3i_z_lJg81XvtT3g_JdXEop1T4WI8pl_zcWDHBzUcIk5zxy88RCxAoFXHBQXxzl-G_tZI3gMyCUt8p0kXfbkaKhXd4r5kgbXQZXTO2goSH0mKdnclMeoDLA9uzipIPpULJbJYhxQhrDGA6q2h_ptIJP9UZMe0Ittzr3pK3YvG4KKdUAV8GFdp7YqUPyFxc2B-iKEogBsw"
+*/
+```
--- src/index.js
+++ src/index.js
@@ -1,29 +1,91 @@
 console.clear() 
 
-import express from 'express'
-import cors from 'cors'
 import dotenv from 'dotenv'
 import chalk from 'chalk'
-import {
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import pg from 'pg'
+import * as lib from './lib.js'
+
+dotenv.config()
+
+const {
+  npm_package_version,
+  PORT,
+  PRIVATE_KEY_PATH,
+  PUBLIC_KEY_PATH,
+  PRIVATE_KEY,
+  PUBLIC_KEY,
+  DATABASE_URL
+} = process.env
+
+const {
   get_jti,
   make_exp,
   get_key_pair,
   get_jwt,
   verify_jwt
-} from './lib.js'
+} = lib
 
-dotenv.config()
+async function verify_jwt_middleware (request, response, next) {
+  try {
+    const authorization = request.headers[`authorization`]
+    const [scheme, jwt] = authorization.split(' ')
+    const [private_key, public_key] = (PRIVATE_KEY && PUBLIC_KEY) ? [PRIVATE_KEY, PUBLIC_KEY] : get_key_pair(PRIVATE_KEY_PATH, PUBLIC_KEY_PATH)
+    const verification = await verify_jwt(jwt, public_key)
+    request.verification = verification
+    next()
+  } catch (error) {
+    console.error(error)
+    response.sendStatus(400)
+  }
+}
 
-const {
-  PORT,
-  PRIVATE_KEY_PATH,
-  PUBLIC_KEY_PATH,
-  PRIVATE_KEY,
-  PUBLIC_KEY
-} = process.env
+// Database -------------------------------------------------------------------------------------------
+
+init_db(DATABASE_URL)
+
+async function init_db (database_url) {
+  try {
+    const pg_client_config = {
+      connectionString: database_url,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+    const pg_client = new pg.Client(pg_client_config)
+    await pg_client.connect()
+    const confirmation = await confirm_db_connection(pg_client)
+    if (confirmation && confirmation.rows.length > 0) {
+      console.log(`Connected to db...`)
+    }
+  } catch (pg_client_connect_error) {
+    console.error(pg_client_connect_error)
+  }
+}
+
+async function confirm_db_connection (pg_client) {
+  try {
+    const query = `select now()`
+    const query_params = []
+    const query_result = await pg_client.query(query, query_params)
+    return Promise.resolve(query_result)
+  } catch (confirm_db_connection_error) {
+    console.error(confirm_db_connection_error)
+    return Promise.reject(confirm_db_connection_error)
+  }
+}
+
+// App -------------------------------------------------------------------------------------------
 
 const app = express()
+app.use(morgan('tiny'))
 app.use(cors({ origin: '*' }))
+app.use((req, res, next) => {
+  res.set('X-Neptune-Aurora-API-Version', `v${npm_package_version}`)
+  next()
+})
 app.listen(PORT || 12000, () => {
   console.log(`Listening on ${PORT || 12000}..`)
 })
@@ -46,26 +108,7 @@ app.get('/api/v2/jwt', async (request, response) => {
   }
 })
 
-async function verify_middleware (request, response, next) {
-  try {
-    const [private_key, public_key] = (PRIVATE_KEY && PUBLIC_KEY) ? [PRIVATE_KEY, PUBLIC_KEY] : get_key_pair(PRIVATE_KEY_PATH, PUBLIC_KEY_PATH)
-    const authorization = request.headers[`authorization`]
-    const [scheme, jwt] = authorization.split(' ')
-    const verification = await verify_jwt(jwt, public_key)
-    request.verification = verification
-    next()
-  } catch (error) {
-    console.error(error)
-    response.sendStatus(400)
-  }
-}
-
-/**
- curl localhost:12000/api/v2/eg-resource \
-  -H "Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlZWY5YmQwNjBhMTNjOGVhNGYzNTY4MWQ4MWU4MWI3MTMyYTUzNTNjYmUzNzBmNjk4NjFkMjM1NzRlZmFlNjk0MzhlMTg4NTA0NzMyNGUyOTQwYmQ2ZGIyMTQ5NzkxYTIiLCJleHAiOjE2NDg3MzM4MDksImlhdCI6MTY0ODczMzcwOX0.X_BCxCR9IVAYw-vPkRPCSgGu9thTowEDGq1_I4VBmjX8BmZDEUuxh6RG9-lk7dHGH87xNa8yX0KacpKiidHF9k4Fz0A3S3ng8c71MT--r8Ku0YAByUEU-nTUmRhrQDErndvl6gwQ39C7H3i_z_lJg81XvtT3g_JdXEop1T4WI8pl_zcWDHBzUcIk5zxy88RCxAoFXHBQXxzl-G_tZI3gMyCUt8p0kXfbkaKhXd4r5kgbXQZXTO2goSH0mKdnclMeoDLA9uzipIPpULJbJYhxQhrDGA6q2h_ptIJP9UZMe0Ittzr3pK3YvG4KKdUAV8GFdp7YqUPyFxc2B-iKEogBsw"
-*/
-
-app.get('/api/v2/eg-resource', verify_middleware, async (request, response) => {
+app.get('/api/v2/eg-resource', verify_jwt_middleware, async (request, response) => {
   response.json([{
     id: 0,
     message: `hi`
