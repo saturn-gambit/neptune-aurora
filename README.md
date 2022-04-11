92731a504c8511f9997199c5ac85e87571546c11
brn <brn.gdsn@gmail.com>
2022-04-11 14:28:01 -0400

    for now

--- README.md
+++ README.md
@@ -8,6 +8,32 @@ oauth2
 ```
 /**
  curl localhost:12000/api/v2/eg-resource \
-  -H "Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlZWY5YmQwNjBhMTNjOGVhNGYzNTY4MWQ4MWU4MWI3MTMyYTUzNTNjYmUzNzBmNjk4NjFkMjM1NzRlZmFlNjk0MzhlMTg4NTA0NzMyNGUyOTQwYmQ2ZGIyMTQ5NzkxYTIiLCJleHAiOjE2NDg3MzM4MDksImlhdCI6MTY0ODczMzcwOX0.X_BCxCR9IVAYw-vPkRPCSgGu9thTowEDGq1_I4VBmjX8BmZDEUuxh6RG9-lk7dHGH87xNa8yX0KacpKiidHF9k4Fz0A3S3ng8c71MT--r8Ku0YAByUEU-nTUmRhrQDErndvl6gwQ39C7H3i_z_lJg81XvtT3g_JdXEop1T4WI8pl_zcWDHBzUcIk5zxy88RCxAoFXHBQXxzl-G_tZI3gMyCUt8p0kXfbkaKhXd4r5kgbXQZXTO2goSH0mKdnclMeoDLA9uzipIPpULJbJYhxQhrDGA6q2h_ptIJP9UZMe0Ittzr3pK3YvG4KKdUAV8GFdp7YqUPyFxc2B-iKEogBsw"
+  -H "Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMzViMTY0YmNhYzI5MWExZmMyYjA0MjdmZmU1NzIzMmM4MjNmNTZkNzllMzQ5OTBiYTM0YjY2NWY2YTEzYzYyZmJhYjUxNTJlYmUwZDQyNmRlY2VjMGE3YzdkZjQxMWMiLCJleHAiOjE2NDg5MDY2OTYsImlhdCI6MTY0ODkwNjU5Nn0.tU0pHDt97ep-gBH0znZfXt2ieA7JGgjGDv9NbjbzKDhLB1XmXwUVAlykQSc75hYdy0Sxo2tDMt-Ik6iD97dx6gZW-pDo2AU3o30MzyKjI36ffSNDxaj-5Zp8C6Ek4FrvWBhbUScswFaBFFDQipAwk4ZhthNhfvqP-u_-xPD3Fvt4X3FvQoYAPIcL3LlEwW_F7zfFozMbCdq_8OZ7D7iPdQvUO7OE82CbR8LhhJovRYOnEGFboLP70DVQCEL_9yGbR5JzYmhzRYMFMjKwHOIdt7w9tPzJZmZIw25EijV2Gs8TKnElTNlRkrlXEL9ol7YsxqDidN4tCoIaLJmZQJd_Wg"
 */
+```
+
+```
+async function test_heroku_api () {
+  const r = await fetch(`https://neptune-aurora.herokuapp.com/api/v2/jwt`)
+  const jwt = await r.text()
+  const Authorization = `Bearer ${jwt}`
+  const headers = { Authorization }
+  const r1 = await fetch(`https://neptune-aurora.herokuapp.com/api/v2/eg-resource`, { headers })
+  const j = await r1.json()
+  console.log(j)
+}
+```
+
+```
+curl localhost:12000/api/v3/authenticate \
+  -v \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -d '{ "username": "0x0000", "password": "0x0000" }'
+```
+
+```
+curl localhost:12000/api/v3/eg-resource \
+  -v \
+  --cookie "jwt=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjB4MDAwMCIsImV4cGlyZXMiOjE2NDk2ODEyNjE0NjIsImlhdCI6MTY0OTY4MDkxMX0.oSnoKaq5GWutigVtGo50x0WQee3LaRE9vCAgLMHVk7Tuv2omjZQBDQ66ZoaUAwvCFQhON4_nc1DTir1TOvBJbQKpvLDLfn7_8KNidX7mwroPtfv0r50cJMQlKWlWDSD0ca2hFZCcNhRYIBx7EpvmVj-4dCQLRVOM6eefL630Roox1W4pAno0tSzFEkIbq_7tF4V2xkiyIwZRBz1g5w9QvCJQHzNkuUne28FpI3mqQtY1NFacT6C7lGKAMH6GSeF2cQWSqhzAEtXOXzRUcLfhIFQPaBFNBJhJdwDuAWrjiZpuoFQy7-njdhpH3vtkNXrwOudLjJgAbV41-JhhyylG0Q; Path=/; HttpOnly; Secure"
 ```
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 console.clear() 
 
+import fs from 'fs'
 import dotenv from 'dotenv'
 import chalk from 'chalk'
 import express from 'express'
+import passport from 'passport'
+import cookie_parser from 'cookie-parser'
+import * as passport_local from 'passport-local'
+import * as passport_jwt from 'passport-jwt'
 import cors from 'cors'
 import morgan from 'morgan'
 import pg from 'pg'
@@ -17,7 +22,8 @@ const {
   PUBLIC_KEY_PATH,
   PRIVATE_KEY,
   PUBLIC_KEY,
-  DATABASE_URL
+  DATABASE_URL,
+  JWT_EXPIRATION_MS
 } = process.env
 
 const {
@@ -28,17 +34,57 @@ const {
   verify_jwt
 } = lib
 
+const {
+  Strategy: PassportLocalStrategy
+} = passport_local
+const {
+  Strategy: PassportJWTStrategy
+} = passport_jwt
+
+// Helpers -------------------------------------------------------------------------------------------
+
+function get_keys () {
+  return (PRIVATE_KEY && PUBLIC_KEY) ? [PRIVATE_KEY, PUBLIC_KEY] : get_key_pair(PRIVATE_KEY_PATH, PUBLIC_KEY_PATH)
+}
+
 async function verify_jwt_middleware (request, response, next) {
   try {
     const authorization = request.headers[`authorization`]
     const [scheme, jwt] = authorization.split(' ')
-    const [private_key, public_key] = (PRIVATE_KEY && PUBLIC_KEY) ? [PRIVATE_KEY, PUBLIC_KEY] : get_key_pair(PRIVATE_KEY_PATH, PUBLIC_KEY_PATH)
+    const [private_key, public_key] = get_keys()
     const verification = await verify_jwt(jwt, public_key)
     request.verification = verification
     next()
   } catch (error) {
     console.error(error)
     response.sendStatus(400)
+  }
+}
+
+const passport_local_strategy_config = {
+  usernameField: 'username',
+  passwordField: 'password',
+  session: false
+}
+
+async function passport_local_strategy_handler (username, password, callback) {
+  // query store for user
+  callback(null, { id: `0x0000`, name: `zero`})
+}
+
+const passport_jwt_strategy_config = {
+  jwtFromRequest: request => {
+    console.log(request.cookies)
+    return request.cookies.jwt
+  },
+  secretOrKey: get_keys()[0]
+}
+
+async function passport_jwt_strategy_handler (jwt_payload, callback) {
+  if (Date.now() > jwt_payload.expires) {
+    return callback('jwt expired')
+  } else {
+    return callback(null, jwt_payload)
   }
 }
 
@@ -79,16 +125,68 @@ async function confirm_db_connection (pg_client) {
 
 // App -------------------------------------------------------------------------------------------
 
+passport.use(new PassportLocalStrategy(passport_local_strategy_config, passport_local_strategy_handler))
+passport.use(new PassportJWTStrategy(passport_jwt_strategy_config, passport_jwt_strategy_handler))
+
 const app = express()
 app.use(morgan('tiny'))
 app.use(cors({ origin: '*' }))
+app.use(express.json())
+app.use(cookie_parser())
 app.use((req, res, next) => {
   res.set('X-Neptune-Aurora-API-Version', `v${npm_package_version}`)
   next()
 })
+app.engine('ntl', (file_path, options, callback) => {
+  fs.readFile(file_path, (error, content) => {
+    if (error) return callback(error)
+    const view = content.toString()
+    const reg_exp = new RegExp('{{npm_package_version}}')
+    const rendered = view.replace(reg_exp, `${options.npm_package_version}`)
+    return callback(null, rendered)
+  })
+})
+app.set('views', './src/views')
+app.set('view engine', 'ntl')
+app.use(passport.initialize())
 app.listen(PORT || 12000, () => {
   console.log(`Listening on ${PORT || 12000}..`)
 })
+
+app.get('/', async (request, response) => {
+  response.render('index', { npm_package_version })
+})
+
+// v3
+
+app.post('/api/v3/authenticate', passport.authenticate('local', { session: false }), async (request, response) => {
+  console.log(request.body, request.user)
+  const username = `0x0000`
+  const payload = {
+    username,
+    expires: Date.now() + parseInt(JWT_EXPIRATION_MS)
+  }
+  try {
+    const [private_key, public_key] = get_keys()
+    const jwt = await get_jwt(private_key, payload)
+    const verification = await verify_jwt(jwt, public_key)
+    console.log(verification)
+    response.cookie('jwt', jwt, { httpOnly: true, secure: true })
+    response.status(200).send({ username })
+  } catch (jwt_signing_error) {
+    console.error(jwt_signing_error)
+    response.status(400).send({ error: jwt_signing_error })
+  }
+})
+
+app.get('/api/v3/eg-resource', passport.authenticate('jwt', { session: false }), async (request, response) => {
+  response.json([{
+    id: 1,
+    message: `hi`
+  }])
+})
+
+// v2
 
 app.get('/api/v2/jwt', async (request, response) => {
   try {
@@ -98,7 +196,7 @@ app.get('/api/v2/jwt', async (request, response) => {
       jti,
       exp
     }
-    const [private_key, public_key] = (PRIVATE_KEY && PUBLIC_KEY) ? [PRIVATE_KEY, PUBLIC_KEY] : get_key_pair(PRIVATE_KEY_PATH, PUBLIC_KEY_PATH)
+    const [private_key, public_key] = get_keys()
     const jwt = await get_jwt(private_key, payload)
     const verification = await verify_jwt(jwt, public_key)
     response.send(jwt)
